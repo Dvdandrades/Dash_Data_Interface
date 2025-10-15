83449abd5e3c572886d9fba4286ca39afb12c298
Dvdandrades <dvdandrades@gmail.com>
2025-10-15 18:15:47 +0200

    add styles.css and improve layout

--- app.py
+++ app.py
@@ -8,46 +8,102 @@ data = (
     .sort_values(by="Year")
 )
 
-app = Dash(__name__)
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Top Movies Analysis"
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Top Movies Analysis"),
-        html.P(
-            children=(
-                "An analysis of top movies with high Metacritic scores and Oscar wins."
-            )
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["Metacritic Score"],
-                        "type": "lines",
+        html.Div(
+            children=[
+                html.P(children="🎬", className="header-emoji"),
+                html.H1(
+                    children="Top Movies Analysis", className="header-title"
+                ),
+                html.P(
+                    children=(
+                        "An analysis of top movies with high Metacritic scores and Oscar wins."
+                ),
+                className="header-description",
+            ),
+        ],
+        className="header",
+    ),
+    html.Div(
+        children=[
+            html.Div(
+                children=dcc.Graph(
+                    id="metacritic-score",
+                    config={"displayModeBar": False},
+                    figure={
+                        "data": [
+                            {
+                                "x": data["Date"],
+                                "y": data["Metacritic Score"],
+                                "type": "lines",
+                                "hovertemplate": (
+                                    "%{y}<extra></extra>"
+                                ),
+                            },
+                        ],
+                        "layout": {
+                            "title": {
+                                "text": "Metacritic Scores Over Time",
+                                "x": 0.05,
+                                "xanchor": "left",
+                            },
+                            "xaxis": {"fixedrange": True},
+                            "yaxis": {
+                                "fixedrange": True,
+                            },
+                            "colorway": ["#17b897"],
+                        },
                     },
-                ],
-                "layout": {
-                    "title": {"text": "Metacritic Scores Over Time"},
-                },
-            }
+                ),
+                className="card",
+            ),
+            html.Div(
+                children=dcc.Graph(
+                    id="oscar-wins",
+                    config={"displayModeBar": False},
+                    figure={
+                        "data": [
+                            {
+                                "x": data["Date"],
+                                "y": data["Oscars Won"],
+                                "type": "lines",
+                            },
+                        ],
+                        "layout": {
+                            "title": {
+                                "text": "Oscar Wins Over Time",
+                                "x": 0.05,
+                                "xanchor": "left",
+                            },
+                            "xaxis": {"fixedrange": True},
+                            "yaxis": {"fixedrange": True},
+                            "colorway": ["#E12D39"],
+                            },
+                        },
+
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["Oscars Won"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {
-                    "title": {"text": "Oscar Wins Over Time"},
-                },
-            },
-        ),
-    ]
+    ],
 )
+       
 
 if __name__ == "__main__":
     app.run(debug=True)
