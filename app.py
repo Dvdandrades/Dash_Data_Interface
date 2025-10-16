9e10854c4b9c47242360df6c9a86a25cc3387f0a
Dvdandrades <dvdandrades@gmail.com>
2025-10-16 11:09:14 +0200

    add interactive dropdown menus

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import pandas as pd
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 
 data = (
     pd.read_csv("top_100_movies_full_best_effort.csv")
-    .query("`Metacritic Score` > 75 and `Oscars Won` > 0")
     .assign(Date=lambda data: pd.to_datetime(data["Year"], format="%Y"))
     .sort_values(by="Year")
 )
+metacritic_score = data["Metacritic Score"].dropna().sort_values().unique()
+oscar_wins = data["Oscars Won"].sort_values().unique()
+
 
 external_stylesheets = [
     {
@@ -22,86 +24,99 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Top Movies Analysis"
 
 app.layout = html.Div(
-    children=[
-        html.Div(
-            children=[
-                html.P(children="🎬", className="header-emoji"),
-                html.H1(
-                    children="Top Movies Analysis", className="header-title"
-                ),
-                html.P(
-                    children=(
-                        "An analysis of top movies with high Metacritic scores and Oscar wins."
-                ),
-                className="header-description",
-            ),
-        ],
-        className="header",
-    ),
-    html.Div(
         children=[
             html.Div(
-                children=dcc.Graph(
-                    id="metacritic-score",
-                    config={"displayModeBar": False},
-                    figure={
-                        "data": [
-                            {
-                                "x": data["Date"],
-                                "y": data["Metacritic Score"],
-                                "type": "lines",
-                                "hovertemplate": (
-                                    "%{y}<extra></extra>"
-                                ),
-                            },
-                        ],
-                        "layout": {
-                            "title": {
-                                "text": "Metacritic Scores Over Time",
-                                "x": 0.05,
-                                "xanchor": "left",
-                            },
-                            "xaxis": {"fixedrange": True},
-                            "yaxis": {
-                                "fixedrange": True,
-                            },
-                            "colorway": ["#17b897"],
-                        },
-                    },
+                children=[
+                    html.P(children="🎬", className="header-emoji"),
+                    html.H1(
+                        children="Top Movies Analysis", className="header-title"
+                    ),
+                    html.P(
+                        children=(
+                            "An analysis of top movies with high Metacritic scores and Oscar wins."
+                    ),
+                    className="header-description",
                 ),
-                className="card",
-            ),
-            html.Div(
-                children=dcc.Graph(
-                    id="oscar-wins",
-                    config={"displayModeBar": False},
-                    figure={
-                        "data": [
-                            {
-                                "x": data["Date"],
-                                "y": data["Oscars Won"],
-                                "type": "lines",
-                            },
-                        ],
-                        "layout": {
-                            "title": {
-                                "text": "Oscar Wins Over Time",
-                                "x": 0.05,
-                                "xanchor": "left",
-                            },
-                            "xaxis": {"fixedrange": True},
-                            "yaxis": {"fixedrange": True},
-                            "colorway": ["#E12D39"],
-                            },
-                        },
-
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Metacritic Score", className="menu-title"),
+                        dcc.Dropdown(
+                            id="metacritic-score-filter",
+                            options=[
+                                {
+                                    "label": score, 
+                                    "value": score,
+                                }
+                                for score in metacritic_score
+                            ],
+                            value=min(metacritic_score),
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Oscars Won", className="menu-title"),
+                        dcc.Dropdown(
+                            id="oscar-wins-filter",
+                            options=[
+                                {
+                                    "label": oscar_win, 
+                                    "value": oscar_win,
+                                }
+                                for oscar_win in oscar_wins
+                            ],
+                            value=min(oscar_wins),
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Year Released", className="menu-title"
+                        ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data["Date"].min().date(), 
+                            max_date_allowed=data["Date"].max().date(),
+                            start_date=data["Date"].min().date(),
+                            end_date=data["Date"].max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="movies-scatter-plot", 
+                        config={"displayModeBar": False}
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="movies-histogram",
+                        config={"displayModeBar": False},
                     ),
                     className="card",
                 ),
             ],
             className="wrapper",
         ),
-    ],
+    ]
 )
        
 
